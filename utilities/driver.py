be906fc6ab03536e9dbebbc8d856d9a844917f2d
Haitham Murad Saud Abu Khaled <231066@ppu.edu.ps>
2024-11-04 23:25:59 +0200

    Some Updates

--- utilities/driver.py
+++ utilities/driver.py
@@ -1,8 +1,20 @@
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+import logging
 
 def get_driver(options=None):
     if options is None:
-        options = webdriver.ChromeOptions()  
-    driver = webdriver.Chrome(options=options)
-    driver.implicitly_wait(10)
-    return driver
+        options = webdriver.ChromeOptions()
+    
+    options.add_argument('--headless')  
+    options.add_argument('--no-sandbox')  
+    options.add_argument('--disable-dev-shm-usage')  
+    options.add_argument('--disable-gpu')  # Optional, for Windows
+
+    try:
+        driver = webdriver.Chrome(options=options)
+        driver.implicitly_wait(10)
+        return driver
+    except WebDriverException as e:
+        logging.error("Failed to initialize WebDriver: %s", e)
+        raise
